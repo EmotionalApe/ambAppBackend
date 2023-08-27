abd3c85f1fdd8f0883914e5ef6b477ee20876b0d
EmotionalApe <maruf1611khan@gmail.com>
2023-08-27 17:22:22 +0530

    bug fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/pay', async (req, res)=> {
         
         const {amount} = Number(req.body);
         // if (!amount) return res.status(400).json({message: 'Amount invalid'})
-        console.log(body)
+        console.log(req.body)
 
         const paymentIntent = await stripe.paymentIntents.create({
             amount: Math.round(25 * 100),
