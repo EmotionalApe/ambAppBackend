f34deab23337ac3ceb468edf5a93e7845b05e1f6
EmotionalApe <maruf1611khan@gmail.com>
2023-08-27 17:39:34 +0530

    readded amount

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ app.post('/pay', async (req, res)=> {
         // const {name} = req.body;
         // if (!name) return res.status(400).json({message: 'Please enter a name'})
         
-        const {amount} = Number(req.body);
+        const {amount} = req.body;
         // if (!amount) return res.status(400).json({message: 'Amount invalid'})
+        console.log(amount)
 
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: Math.round({amount} * 100),
+            amount: Math.round(25 * 100),
             currency: 'INR',
             automatic_payment_methods: {enabled: true,},
         })
